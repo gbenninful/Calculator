0f6a9368f957968f33d7c5c11b2d2b98dfed51bd
gbenninful <enninful@hotmail.com>
2014-02-17 03:38:14 -0800

    Fixed the add function problem of concatenating instead adding ( used parseFloat)

--- Calculator.js
+++ Calculator.js
@@ -2,38 +2,31 @@
 
     "use strict";
 
-    var firstNumber = document.getElementsByName("firstNumber");
-    var secondNumber = document.getElementsByName("secondNumber");
-    var showTotal = document.getElementById("display");
+    var firstNumber = document.getElementsByName("firstNumber"),
+        secondNumber = document.getElementsByName("secondNumber"),
+        showTotal = document.getElementById("display");
 
     //Add function
     var add = function () {
-        //if ((firstNumber.value !== "undefined") && (secondNumber.value !== "undefined")) {
-
-        //    var result = firstNumber.value + secondNumber.value;
-        //    showTotal.innerHTML = result;
-        //} else {
-        //  console.log("I've got some work to do!");
-        //}
-        var result = (firstNumber[0].value) + (secondNumber[0].value);
-        document.getElementById("display").innerHTML = result;
-        console.log(typeof result)
+        firstNumber = parseFloat(firstNumber[0].value);
+        secondNumber = parseFloat(secondNumber[0].value);
+        var result = firstNumber + secondNumber;
+        showTotal.innerHTML = result;
     };
 
     var subtract = function () {
         var result = (firstNumber[0].value) - (secondNumber[0].value);
-        document.getElementById("display").innerHTML = result;
-        console(result);
+        showTotal.innerHTML = result;
     };
 
     var multiply = function () {
         var result = (firstNumber[0].value) * (secondNumber[0].value);
-        document.getElementById("display").innerHTML = result;
+        showTotal.innerHTML = result;
     };
 
     var divide = function () {
         var result = (firstNumber[0].value) / (secondNumber[0].value);
-        document.getElementById("display").innerHTML = result;
+        showTotal.innerHTML = result;
     };
 
     var calculate = function () {
